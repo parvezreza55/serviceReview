7285517d81421143c2ed219eb960651e6be4ee98
parvez <parvezreza112@gmail.com>
2025-06-17 23:52:50 +0600

    remove error

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import errorImage from "../assets/images/errorpage.jpg";
-import Navbar from "./Navbar";
 
 const ErrorPage = () => {
   return (
     <div>
-      <Navbar></Navbar>
       <div className="flex flex-col justify-center items-center py-24">
         <img className="rounded-2xl" src={errorImage} alt="" />
         <h1 className="text-xl md:text-4xl text-center font-bold mt-10">
